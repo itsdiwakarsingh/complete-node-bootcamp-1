976e886699471b085b8b83cec17c0b56674d25f9
Diwakar Singh <101459162+itsdiwakarsingh@users.noreply.github.com>
2025-08-18 23:35:26 +0530

    06 Express building Natours API

---
+++ 4-natours/starter/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res
+    .status(404)
+    .json({ message: 'Hello from the server side!', app: 'Natours' });
+});
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}....`);
+});
