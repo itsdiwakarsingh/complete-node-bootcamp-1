ffebf17a0885bbc596051b301e7521617c9d7168
Diwakar <101459162+itsdiwakarsingh@users.noreply.github.com>
2025-09-11 19:35:17 +0530

    update

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,15 +1,33 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
+
 const app = express();
+
+// 1) MIDDLEWARES
+app.use(morgan('dev'));
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware 👋');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`),
 );
 
+// 2) ROUTE HANDLERS
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
+    requestAt: req.requestTime,
     results: tours.length,
     data: {
       tours,
@@ -94,6 +112,7 @@ const deleteTour = (req, res) => {
 // app.patch('/api/v1/tours/:id', updateTour);
 // app.delete('/api/v1/tours/:id', deleteTour);
 
+// 3) ROUTES
 app.route('/api/v1/tours').get(getAllTours).post(createTour);
 
 app
@@ -102,6 +121,9 @@ app
   .patch(updateTour)
   .delete(deleteTour);
 
+app.route('/api/v1/')
+
+// 4) START SERVER
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}....`);
---
+++ 4-natours/starter/shub.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main ()
+{
+    printf("MY NAME IS SHUBHAM");
+    return 0;
+
+}
